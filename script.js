1aa11fdaf5c417e3cc946a3933bb53446fb8f945
aplyd <austin@ftacnik.com>
2019-07-24 16:29:04 -0700

    Seperate html/js files. Add parenthesis to playRound() for readability. Create game() to keep track of score.

---
+++ script.js
@@ -0,0 +1,37 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function game() {
+
+    function playRound(playerSelection, computerSelection) {
+        if (computerSelection == playerSelection) {
+            return 'tie game';
+        } else if ((computerSelection == 'paper' && playerSelection == 'rock') || (computerSelection == 'scissors' && playerSelection == 'paper') || (computerSelection == 'rock' && playerSelection == 'scissors')) {
+            computerScore++;
+            return 'computer wins';
+        } else if ((computerSelection == 'scissors' && playerSelection == 'rock') || (computerSelection == 'rock' && playerSelection == 'paper') || (computerSelection == 'paper' && playerSelection == 'scissors')) {
+            playerScore++;
+            return 'player wins';
+        }
+    }
+
+    function computerPlay() {
+        let randomNumber = Math.floor(Math.random() * 3 + 1);
+        if (randomNumber == 1) {
+            return 'rock';
+        } else if (randomNumber == 2) {
+            return 'paper';
+        } else if (randomNumber == 3) {
+            return 'scissors';
+        }
+    }
+
+
+let playerSelection = 'scissors';
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+console.log('p: ' + playerScore);
+console.log('c: ' + computerScore);
+}
+
+game();
