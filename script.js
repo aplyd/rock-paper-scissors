900958161ed3f6255792f83f1bc5377bd0fa91ec
aplyd <austin@ftacnik.com>
2019-08-06 18:49:28 -0700

    add round increment

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 let playerScore = 0;
 let computerScore = 0;
+let roundIncrement = 0;
 
 function game() {
+
+    function timer() {
+        let interval = 3;
+        let counter = document.querySelector('#countdown');
+        counter.innerHTML = '';
+        counter.style.fontSize = '60px';
+        setInterval(function() {
+            document.querySelector('#countdown').textContent = interval--;
     
-
-
-
+            if (interval < 0) {
+                document.querySelector('#countdown').textContent = '';
+            }
     
+        }, 500);
+    
+    }
+
+    timer();
 
     function playRound(playerSelection, computerSelection) {
 
         if (computerSelection == playerSelection) {
-            return 'tie game';
+            document.querySelector('#countdown').textContent = 'tie game';
+
         } else if ((computerSelection == 'paper' && playerSelection == 'rock') || 
                     (computerSelection == 'scissors' && playerSelection == 'paper') || 
                     (computerSelection == 'rock' && playerSelection == 'scissors')) {
             computerScore++;
-            return 'computer wins';
+            document.querySelector('#countdown').textContent = 'computer wins';
+
         } else if ((computerSelection == 'scissors' && playerSelection == 'rock') || 
                     (computerSelection == 'rock' && playerSelection == 'paper') || 
                     (computerSelection == 'paper' && playerSelection == 'scissors')) {
             playerScore++;
-            return 'player wins';
+            document.querySelector('#countdown').textContent = 'player wins';
+
         } else {
             return 'o h  n o';
         }
     }
 
+    //generate random num and assign to selection
     function computerPlay() {
         let randomNumber = Math.floor(Math.random() * 3 + 1);
         if (randomNumber == 1) {
@@ -36,47 +54,44 @@ function game() {
         } else if (randomNumber == 3) {
             return 'scissors';
         }
+
     }
 
 
+    let computerSelection = computerPlay();
 
-let computerSelection = computerPlay();
-//alert(playRound(playerSelection, computerSelection));
-console.log('p: ' + playerScore);
-console.log('c: ' + computerScore);
+    //
+    playRound(playerSelection, computerSelection);
+        
+    
+        //need to update DOM with player score
+        console.log('p: ' + playerScore);
+
+        //update DOM with computer score
+        console.log('c: ' + computerScore);
+
+    let roundOptions = ['ROUND ONE', 'ROUND TWO', 'ROUND THREE', 'ROUND FOUR', 'ROUND FIVE', 'GAME OVER']
+    let roundDisplay = document.querySelector('#round');
+    roundDisplay.textContent = roundOptions[roundIncrement];
+    roundIncrement++;
 }
 
 
-
-function timer() {
-    let interval = 3;
-    let counter = document.querySelector('#countdown');
-    counter.innerHTML = '';
-    counter.style.fontSize = '60px';
-    setInterval(function() {
-        document.querySelector('#countdown').textContent = interval--;
-
-        if (interval < 0) {
-            document.querySelector('#countdown').textContent = '';
-        }
-
-    }, 1000);
-}
 
 function playRock() {
     playerSelection = "rock";
-    timer();
+    
     game();
 }
 
 function playPaper() {
     playerSelection = "paper";
-    timer();
+    
     game();
 }
 
 function playScissors() {
     playerSelection = "scissors";
-    timer();
+    
     game();
 }
