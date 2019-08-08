f86c74d1d91493de8d88b1b5a0722623dfbe2d45
aplyd <austin@ftacnik.com>
2019-08-07 18:52:00 -0700

    update dom with round points

--- script.js
+++ script.js
@@ -1,47 +1,60 @@
 let playerScore = 0;
 let computerScore = 0;
-let roundIncrement = 1;
+let roundIncrement = 0;
+
+// let counter = document.querySelector('#countdown');
+//     counter.style.fontSize = '20px';
+    
 
 function game() {
 
-    function timer() {
-        let interval = 3;
-        let counter = document.querySelector('#countdown');
-        counter.innerHTML = '';
-        counter.style.fontSize = '60px';
-        setInterval(function() {
-            document.querySelector('#countdown').textContent = interval--;
-    
-            if (interval < 0) {
-                document.querySelector('#countdown').textContent = '';
-            }
-    
-        }, 500);
-    
+    let roundOptions = ['ROUND ONE', 'ROUND TWO', 'ROUND THREE', 'ROUND FOUR', 'ROUND FIVE', 'GAME OVER']
+    let roundDisplay = document.querySelector('#round');
+    roundDisplay.textContent = roundOptions[roundIncrement];
+    roundIncrement++;
+        if (roundIncrement >= 6) {
+            roundIncrement = 0;
+            playerScore = 0;
+            computerScore = 0;
+        }
+
+    if (playerScore == 4 || computerScore == 4) {
+        roundDisplay.textContent = roundOptions[5];
     }
 
-    timer();
+    //TODO attempt at countdown but can't get it to work correctly
+    // let interval = 3;
+
+    // function timer() {    
+    //     setInterval(function() {
+    //         document.querySelector('#countdown').textContent = interval;
+    //             --interval;
+    //                 if (interval <= 0) {
+    //                 document.querySelector('#countdown').textContent = '';
+    //                 clearTimeout(timer);
+    //                 }
+    //     }, 1000);
+    // }
+
+    // timer();
 
     function playRound(playerSelection, computerSelection) {
 
         if (computerSelection == playerSelection) {
-            document.querySelector('#countdown').textContent = 'tie game';
+            document.querySelector('#countdown').textContent = 'tie';
 
         } else if ((computerSelection == 'paper' && playerSelection == 'rock') || 
                     (computerSelection == 'scissors' && playerSelection == 'paper') || 
                     (computerSelection == 'rock' && playerSelection == 'scissors')) {
             computerScore++;
-            document.querySelector('#countdown').textContent = 'computer wins';
+            document.querySelector('#countdown').textContent = 'computer';
 
         } else if ((computerSelection == 'scissors' && playerSelection == 'rock') || 
                     (computerSelection == 'rock' && playerSelection == 'paper') || 
                     (computerSelection == 'paper' && playerSelection == 'scissors')) {
             playerScore++;
-            document.querySelector('#countdown').textContent = 'player wins';
-
-        } else {
-            return 'o h  n o';
-        }
+            document.querySelector('#countdown').textContent = 'player';
+        } 
     }
 
     //generate random num and assign to selection
@@ -58,40 +71,38 @@ function game() {
     }
 
 
+    //assign computer random selection and pass to game engine
     let computerSelection = computerPlay();
+    playRound(computerSelection, playerSelection);
 
-    //
-    playRound(playerSelection, computerSelection);
-        
-    
-        //need to update DOM with player score
-        console.log('p: ' + playerScore);
+    const playerpoints = document.querySelector('#playerpoints');
+    const computerpoints = document.querySelector('#computerpoints');
 
-        //update DOM with computer score
-        console.log('c: ' + computerScore);
+    const pointsToMarks = [' ' ,' | ', ' | | ', ' | | | ', ' | | | | ', ' | | | | | '];
 
-    let roundOptions = ['ROUND ONE', 'ROUND TWO', 'ROUND THREE', 'ROUND FOUR', 'ROUND FIVE', 'GAME OVER']
-    let roundDisplay = document.querySelector('#round');
-    roundDisplay.textContent = roundOptions[roundIncrement];
-    roundIncrement++;
+    playerpoints.textContent = pointsToMarks[playerScore];
+    computerpoints.textContent = pointsToMarks[computerScore];
+
+    console.log('c: ' + computerScore);
+    console.log('p: ' + playerScore);
 }
 
 
 
 function playRock() {
+    document.querySelector('#countdown').textContent = '';
     playerSelection = "rock";
-    
     game();
 }
 
 function playPaper() {
+    document.querySelector('#countdown').textContent = '';
     playerSelection = "paper";
-    
     game();
 }
 
 function playScissors() {
+    document.querySelector('#countdown').textContent = '';
     playerSelection = "scissors";
-    
     game();
 }
